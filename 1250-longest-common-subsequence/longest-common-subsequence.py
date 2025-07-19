7920f16c34f5432a0632ac7e955de7cc7f70333c
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2025-07-19 21:11:09 +0530

    Time: 481 ms (49.68%) | Memory: 42.7 MB (72.05%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -2,20 +2,14 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        momo = {}
-        def dp(i,j):
-            if i == -1 or j==-1:
-                return 0
-            if (i,j) in momo:
-                return momo[(i,j)]
-            
-            
-            if text1[i]==text2[j]:
-                c = 1+ dp(i-1,j-1)
-            else:
-                c = max(dp(i-1,j),dp(i,j-1))
-            momo[(i,j)]=c
-            return c
-        return dp(n-1,m-1)
-        
-        
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,n+1,1):
+            for j in range(1,m+1,1):
+                if text1[i-1]==text2[j-1]:
+                    c=1+dp[i-1][j-1]
+                else:
+                    c= max(dp[i-1][j],dp[i][j-1])
+                dp[i][j]=c
+        return dp[n][m]
+
+      
