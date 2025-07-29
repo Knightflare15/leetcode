cfb9c8efcce0579f8657f2ad16d41cf6c04b1869
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2025-07-29 22:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (71.55%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,5 +1,8 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        d = {}
+        for i in range(len(order)) :
+            d[order[i]] = i
         for i in range(1,len(words)):
             j = 0
             while j < len(words[i]) and j<len(words[i-1]):
@@ -8,7 +11,7 @@ class Solution:
                 j+=1
             if j==len(words[i]) and words[i]!=words[i-1]:
                 return False
-            if j<len(words[i]) and j<len(words[i-1]) and order.find(words[i][j])<order.find(words[i-1][j]):
+            if j<len(words[i]) and j<len(words[i-1]) and d[words[i][j]]<d[words[i-1][j]]:
                 return False
         return True
 
