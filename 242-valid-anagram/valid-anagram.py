e6cfdfb06dbec9df0852bd9053391078967865c4
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2025-12-16 20:04:52 +0530

    Time: 19 ms (14.89%) | Memory: 18.5 MB (18.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mapp = dict()
-        for i in s:
-            mapp[i] = mapp.get(i,0)+1
-        for i in t:
-            mapp[i] = mapp.get(i,0)-1
-            if mapp[i]==0:
-                mapp.pop(i,None)
-        if not mapp:
+        s = sorted(s)
+        t = sorted(t)
+
+        if s==t:
             return True
         return False
             
