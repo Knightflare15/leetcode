2f84c9ed044239f09576230f5ac59ff3bee6f289
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-11 18:35:55 +0530

    Time: 78 ms (6.08%) | Memory: 21.9 MB (96.62%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,37 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
-    height = 0
-    head = None
-    def hasNode(self,root,node):
-        if not root:
-            return False
-        if root == node:
-            return True
-        K = False
-        if root.left:
-            K = K or self.hasNode(root.left,node)
-        if root.right:
-            K = K or self.hasNode(root.right,node)
-        return K
-    def traversal(self,p,q,root,h):
-        if not root:
-            return
-        key = self.hasNode(root,p) and self.hasNode(root,q)
-        self.traversal(p,q,root.right,h+1)
-        self.traversal(p,q,root.left,h+1)
-        if key  == True:
-            if h>self.height:
-                self.head = root
-                self.height = h
-        
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-            self.height = -1
-            self.head = None
-            self.traversal(p,q,root,0)
-            return self.head
+    def lowestCommonAncestor(self, root, p, q):
+
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        return left if left else right
