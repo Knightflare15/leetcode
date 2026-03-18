36f4ed79367026b3246f2aa5396974fffed197d0
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-18 15:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (65.45%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -4,13 +4,14 @@ class Solution:
         res = [0]*(l+1)
         res[0] = 1
         for i in range(l):
-            if i>0 and s[i]!="0" and 10<int(s[i-1:i+1])<=26 :
+            if i>0 and s[i] == "0" and s[i-1:i+1] in {"10","20"}:
+                res[i+1] = res[i-1]
+                continue
+            if i>0 and 10<int(s[i-1:i+1])<=26 :
                 res[i+1] = res[i] + res[i-1]
                 continue
                 
-            elif i>0 and s[i] == "0" and s[i-1:i+1] in {"10","20"}:
-                res[i+1] = res[i-1]
-                continue
+            
             elif s[i] == "0":
                 return 0
             else:
