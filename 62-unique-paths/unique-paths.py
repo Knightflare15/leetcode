409d88ee78d93680fd82a779a66e8e8ee6bf2e71
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-19 01:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.93%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,27 +1,9 @@
-import math
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 0 and n == 0:
-            return 0
-        q = deque([[0,0]])
-        moves = {(0,1),(1,0)}
-        mat = [ [0]*(n+1) for _ in range(m+1)]
-        if n>0:
-            mat[0][1] = 1
-        else:
-            mat[1][0] = 1
-        while q:
+        dp = [1] * n
 
-            pos = q.popleft()
-            i = pos[0]
-            j = pos[1]
-            if mat[i+1][j+1]>0:
-                continue
-            mat[i+1][j+1] = mat[i][j+1] + mat[i+1][j]
-            if i+1<m :
-                q.append([i+1,j])
-            if j+1<n :
-                q.append([i,j+1])
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j - 1]
 
-        return mat[m][n]
-            
+        return dp[-1]
