8e3b0bcd9be9447df6bdbc13d461c3b262904738
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-18 16:19:48 +0530

    Time: 541 ms (47.43%) | Memory: 19.5 MB (89.77%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -10,8 +10,6 @@ class Solution:
                 continue
             for itr in coins:
                 if k-itr >=0:
-                   dp[k] = min(dp[k],dp[k-itr])
-            if dp[k] != 0:
-                dp[k]+=1
+                   dp[k] = min(dp[k],dp[k-itr]+1)
 
         return dp[amount] if dp[amount]!=float('inf') else -1 
