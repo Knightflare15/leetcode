d2e129035f7398aac1c93c55016319d55b57648d
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-28 01:00:01 +0530

    Time: 23 ms (79.24%) | Memory: 20.6 MB (92.67%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,16 +1,20 @@
-from collections import Counter
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        counter = Counter(tasks)
-        maxF= max(counter[i] for i in counter)
-        maxC = 0 
-        for i in counter:
-            if counter[i] == maxF:
-                maxC+=1
-        part_count = maxF - 1
-        part_length = n + 1
-        empty_slots = part_count * part_length + maxC
-        return max(len(tasks),empty_slots)
+        d = {}
+        for i in tasks:
+            d[i] = d.get(i,0)+1
+        sorted_dict = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        maxi = sorted_dict[0][1]
+        count_max = 1
+        for i in range(1,len(sorted_dict)):
+            if sorted_dict[i][1] != maxi:
+                break
+            count_max+=1
+                
+        minimum = (n+1)*(maxi-1)+count_max
+        
+        return max(len(tasks),minimum)
                 
 
 
