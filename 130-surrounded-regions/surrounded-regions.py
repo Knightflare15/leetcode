a4c96f87af08f8c84a0cfa73c2354a1f0d702e6b
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-30 02:04:23 +0530

    Time: 4 ms (69.65%) | Memory: 22.3 MB (68.26%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,33 +1,34 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        moves = [[-1,0],[1,0],[0,1],[0,-1]]
-        visitededge = set()
-        m = len(board)
-        n = len(board[0])
-        def dfs(i,j):
-            if i<0 or j<0 or i>=m or j >= n :
-                return False
-            if board[i][j] == "X" or (i,j) in visited:
-                return True
-            visited.add((i,j))
-            res = True
-            for x,y in moves:
-                ni = x+i
-                nj = y+j
-                res = res and dfs(ni,nj)
-            return res
-        visited = set()
+        if not board:
+            return
+
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != "O":
+                return
+            
+            board[i][j] = "T"
+
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        # Step 1: mark boundary-connected O's
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n-1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m-1, j)
+
+        # Step 2: flip + restore
         for i in range(m):
             for j in range(n):
-                found = False
                 if board[i][j] == "O":
-                    found = dfs(i,j)
-                if found:
-                    for itrX,itrY in visited:
-                        board[itrX][itrY] = "X"
-                visited.clear()
-                
-        return
+                    board[i][j] = "X"
+                elif board[i][j] == "T":
+                    board[i][j] = "O"
