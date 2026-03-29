c4ff0a40b2735020073470124ff40fca1d0d8706
Knightflare <75769994+Knightflare15@users.noreply.github.com>
2026-03-30 02:01:07 +0530

    Time: 78 ms (5.04%) | Memory: 36.9 MB (5.03%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        moves = [[-1,0],[1,0],[0,1],[0,-1]]
+        visitededge = set()
+        m = len(board)
+        n = len(board[0])
+        def dfs(i,j):
+            if i<0 or j<0 or i>=m or j >= n :
+                return False
+            if board[i][j] == "X" or (i,j) in visited:
+                return True
+            visited.add((i,j))
+            res = True
+            for x,y in moves:
+                ni = x+i
+                nj = y+j
+                res = res and dfs(ni,nj)
+            return res
+        visited = set()
+        for i in range(m):
+            for j in range(n):
+                found = False
+                if board[i][j] == "O" and (i,j) not in visitededge:
+                    found = dfs(i,j)
+                if found:
+                    for itrX,itrY in visited:
+                        board[itrX][itrY] = "X"
+                else:
+                    for itr in visited:
+                        visitededge.add(itr)
+                visited.clear() 
+        return
